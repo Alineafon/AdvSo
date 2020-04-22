e5f9ce8bba90ff1bf84d3ed5fdc781d355c98d3c
Aline Fonseca <63132876+Alineafon@users.noreply.github.com>
2020-04-22 18:24:34 -0300

    Update teste_script_ibex.js

--- data_includes/teste_script_ibex.js
+++ data_includes/teste_script_ibex.js
@@ -11,21 +11,32 @@ newTrial("welcome",
     ,
     newText("<p>Neste experimento, voc&ecirc; vai ouvir uma frase e depois deve escolher a melhor op&ccedil;&atilde;o de interpreta&ccedil;&atilde;o para ela.</p>")
     ,
-    newText("<p>Aperte <strong>A</strong> no teclado para a op&ccedil;&atilde;o da esquerda, ou <strong>L</strong> para a op&ccedil;&atilde;o da direita.</p>")
+    newText("<p> Sua tarefa &eacute;: ou&ccedil;a a frase e depois clique no bot&atilde;o &quot;Pr&oacute;ximo&quot;,</p>")
     ,
-    newText("<p>Por favor, escreva se nome e aperte Start para iniciar.</p>")
+    newText("<p>Escolha uma das op&ccedil;&otilde;es de interpreta&ccedil;&atilde;o, apertando: </p>")
+    ,
+    newText("a tecla <strong>A</strong> para a op&ccedil;&atilde;o da esquerda, ou a tecla <strong>L</strong> para a op&ccedil;&atilde;o da direita.
+    ,
+    newText("<p>Por favor, escreva se NOME na caixa abaixo.</p>")
     ,
     newTextInput("Nome")
     .css("font-size","1em")
         .print()
     ,
-      newTextInput("Idade")
+    newText("<p>Escreva sua IDADE na caixa abaixo,</p>")
+    .print()
+    ,
+    newTextInput("Idade")
     .css("font-size","1em")
         .print()
         ,
-         newVar("AGE")
-        .global()
-        .set( getTextInput("Idade") )
+    newText("<p>Agora, escreva sua ESCOLARIDADE na caixa abaixo, e clique em &quot;Start&quot; para come&ccedil;ar.</p>")
+    .print()
+    ,
+    newTextInput("Escolaridade")
+    .css("font-size","1em")
+        .print()
+  
     ,
     newButton("Start")
     .css("font-size","1em")
@@ -34,10 +45,19 @@ newTrial("welcome",
     ,
     newVar("ID")
         .global()
-        .set( getTextInput("Nome") )
+        .set( getTextInput("Nome"))
+    ,
+         newVar("AGE")
+        .global()
+        .set( getTextInput("Idade") )
+    ,
+         newVar("SCHOOL")
+        .global()
+        .set( getTextInput("Escolaridade") )
 )
     .log( "ID" , getVar("ID") )
     .log( "AGE" , getVar("AGE") )
+    .log( "SCHOOL" , getVar("SCHOOL") )
 
 // This Template command generates as many trials as there are rows in myTable.csv
 Template( "SoAdv_ibex.csv" ,
@@ -45,33 +65,35 @@ Template( "SoAdv_ibex.csv" ,
     variable => newTrial( "experiment",
         newAudio(variable.Audiofile)
          .play()
-         ,
-        newImage("icone_alto_falante.png")
-        //.print()
         ,
-        newButton("Pr&oacute;ximo")
-        .css("font-size","1em")
-        //.print()
-        .log()
-        .wait()
-        ,
-     newCanvas(1200,600)
-        .add(550 , 0 , getImage("icone_alto_falante.png") )
-        .add(550 , 300 , getButton("Pr&oacute;ximo") )
-        .print()
+    newImage("alto_falante_icone.png")
+    .size(90, 90)
+    .print()
+ ,
+    newButton("Pr&oacute;ximo")
+    .css("font-size","1em")
+    .center()
+    .print()
+    .log()
+    .wait()
+    .remove()
     ,
+    getImage("alto_falante_icone.png")
+    .remove()
+    ,
+    
     newText("A",variable.OptionA)
-    .css("font-size","1.4em")
+    .css("font-size","1.2em")
     //.print
     ,
     newText("L",variable.OptionB)
-    .css("font-size","1.4em")
+    .css("font-size","1.2em")
     //.print   
     ,
-     newCanvas(1600,700)
-    .add(   0 , 100 , getText("A") )
-    .add( 800 , 100 , getText("L") )
-        .print()
+     newCanvas(1400,700)
+    .add(   50 , 100 , getText("A") )
+    .add( 750 , 100 , getText("L") )
+    .print()
     ,
 
     //newKey("AL")
@@ -79,8 +101,8 @@ Template( "SoAdv_ibex.csv" ,
     .add( getText("A") , getText("L") )
     .keys(          "A"    ,          "L"   )
     .log()
-     .wait() 
-
+    .wait() 
+     
     ))
     
 
@@ -90,9 +112,10 @@ newTrial( "final" ,
     ,
      newText("<p> Aperte o bot&atilde;o &quot;Salvar&quot; para gravar suas respostas</p>")
         .print()
-        ,
+    ,
     newButton("SALVAR")
     .css("font-size","1em")
         .print()
         .wait()
  )
+
